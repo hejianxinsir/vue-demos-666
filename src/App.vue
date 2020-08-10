82d9328e09d86b46eecfbb27a49d834f3b10640c
hejianxin <hejianxinsir@gmail.com>
2020-08-10 11:30:33 +0800

    第一次 build

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
 
 <script>
 
-import HelloWorld from './components/HelloWorld.vue';
+// import HelloWorld from './components/HelloWorld.vue';
 // import {EventBus} from './EventBus/EventBus.js';
 
 export default {
@@ -23,7 +23,7 @@ export default {
     }
   },
   components: {
-    HelloWorld
+    // HelloWorld
   },
   methods: {
     // receive(data){
